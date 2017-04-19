18fcc2a3721b07674373174fc18f75d76b905ad9
Kai Curry <kai@cape.io>
2017-04-19 17:23:29 -0400

    add back in route finder

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import init from './init'
 
 export { default as Root } from './Root'
 export { default as Router } from './Router'
+export * from './routing'
 export default init
--- src/routing.js
+++ src/routing.js
@@ -2,23 +2,28 @@ import { selectActiveKeyDefault } from 'redux-history-sync'
 import { flow, partial, property } from 'lodash'
 import { createSelector } from 'reselect'
 import { getProps, select } from 'cape-select'
+import { findRoute, selectRoutes } from 'location-info'
 
 export const routeParamsPath = ['route', 'params']
 export const routeParams = flow(getProps, property(routeParamsPath))
 export const routeParam = partial(select, routeParams)
 
 // We are using the redux-history-sync to put location into state.
-function routeInfoSelector(history) {
+function routeInfoSelector(routes, history) {
   if (!history) return history
   return {
+    ...findRoute(routes, history),
     history,
     // Location object gets sent to locationInfo
-    route: {},
   }
 }
 // Pass in the state object and return some info about a "route".
 // selectActiveKeyDefault() is a helper function to grab the current location info.
-const routingSelector = createSelector(selectActiveKeyDefault, routeInfoSelector)
+const routingSelector = createSelector(
+  selectActiveKeyDefault,
+  selectRoutes,
+  routeInfoSelector
+)
 export const getRoute = select(routingSelector, 'route')
 export const getRouteId = select(getRoute, 'id')
 export default routingSelector
